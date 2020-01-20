d0939d1c951753934b14c37e1a216e3e5e09c2f8
yurong <yurong@mafengwo.com>
2020-01-20 16:50:24 +0800

    读取配置+redis类封装

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module wssgo
 go 1.13
 
 require (
+	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/gorilla/websocket v1.4.1
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/viper v1.6.2
 )
--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-    "wssgo/wsServer"
-    "wssgo/httpServer"
+    //"wssgo/wsServer"
+    //"wssgo/httpServer"
+    "wssgo/config"
+    "fmt"
+    "flag"
+    "wssgo/libs"
+    "log"
+)
 
+var (
+    Env = flag.String("env", "prod", "env flag")
 )
 
 func main() {
-    go httpServer.Init();
-    wsServer.Init();
+    flag.Parse()
+    fmt.Println(*Env)
+    config.InitServiceConfig(*Env)
+    localIp, err := libs.GetLocalIp()
+    if err != nil {
+        log.Fatal("get local ip error:" + err.Error())
+    }
+    config.ServiceConf.LocalIp = localIp
+
+    //redisConf := libs.GetDefaultRedisConf()
+    //redisCli := libs.NewRedis(redisConf)
+    //redisCli.Connect()
+    ////redisCli.Set("test", "hello", 1)
+    //rVal, err:= redisCli.Get("test")
+    //fmt.Println(rVal)
+
+    //go httpServer.Init();
+    //wsServer.Init();
 }
